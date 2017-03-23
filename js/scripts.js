69148744ddb80d9b874717adb411ac01eb086029
Katie Borel and Zach Bastian <zach@>
2017-03-23 10:37:58 -0700

    It will add a click and hide functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,30 @@
 $(function() {
-var animal= prompt("Type in Dragon, Unicorn, or Centaur to go on a Learning Adventure!!!").toUpperCase();
+
+  $("#clickable").click(function(){
+    var animal = prompt("Type in Dragon, Unicorn, or Centaur to go on a Learning Adventure!!!").toUpperCase();
+
+
+  // var animal = prompt("Type in Dragon, Unicorn, or Centaur to go on a Learning Adventure!!!").toUpperCase();
+
 
   if (animal==="DRAGON"){
+    $(".unicorn, .centaur, .nothing").hide();
     $(".dragon").show();
   }
   else if (animal==="UNICORN"){
+    $(".dragon, .centaur, .nothing").hide();
     $(".unicorn").show();
   }
   else if (animal==="CENTAUR"){
+    $(".unicorn, .dragon, .nothing").hide();
     $(".centaur").show();
   }
   else   {
+    $(".unicorn, .dragon, .centaur").hide();
     $(".nothing").show();
   }
 
 
-
+});
 
 });
